b7056d37042973db3d9ee724558d8537d481f390
Benno Zeeman <bzeeman@live.nl>
2018-12-06 20:15:47 +0100

    feat: split RDF functionality into separate class

--- src/Identity.ts
+++ src/Identity.ts
@@ -1,30 +1,12 @@
-import Md from './Md';
+import RdfMd from './RdfMd';
 
 import rdflib from 'rdflib';
 
-import { CONSTANTS } from '@maidsafe/safe-node-app';
-import { MutableData } from '@maidsafe/safe-node-app/src/api/mutable';
 
-
-export default class Identity extends Md {
+export default class Identity extends RdfMd {
   public name: string = '';
 
-  public md: MutableData | undefined;
-
-  public graph: any;
-
   public async commit() {
-    if (this.md === undefined) {
-      this.md = await this.app.mutableData.newPublic(this.xor, this.tag);
-
-      const perms = await this.app.mutableData.newPermissions();
-      const key = await this.app.crypto.getAppPubSignKey();
-      const set = ['Insert', 'Update', 'Delete', 'ManagePermissions'];
-      await perms.insertPermissionSet(key, set);
-
-      await this.md.put(perms, CONSTANTS.MD_ENTRIES_EMPTY);
-    }
-
     this.graph = rdflib.graph();
 
     const FOAF = rdflib.Namespace('https://xmlns.com/foaf/0.1/');
@@ -36,20 +18,11 @@ export default class Identity extends Md {
     this.graph.add(rdflib.sym(this.url + '#me'), RDF('type'), FOAF('Person'));
     this.graph.add(rdflib.sym(this.url + '#me'), FOAF('name'), this.name);
 
-
-    const rdf = this.md.emulateAs('RDF');
-    rdf.graphStore = this.graph;
-    rdf.id = this.url;
-    await rdf.commit();
+    return await super.commit();
   }
 
   public async fetch() {
-    this.md = await this.app.mutableData.newPublic(this.xor, this.tag);
-
-    const rdf = this.md.emulateAs('RDF');
-
-    await rdf.nowOrWhenFetched();
-    this.graph = rdf.graphStore;
+    await super.fetch();
 
     const FOAF = rdflib.Namespace('https://xmlns.com/foaf/0.1/');
     const name = this.graph.any(rdflib.sym(this.url + '#me'), FOAF('name'), null);
