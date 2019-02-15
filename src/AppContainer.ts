393baabbce6f894a6f75ec54cf73944158c672d1
Benno Zeeman <bzeeman@live.nl>
2019-02-15 12:19:29 +0100

    fix/tests: Fix incorrect updating of existing key

--- src/AppContainer.ts
+++ src/AppContainer.ts
@@ -1,5 +1,5 @@
 import { SAFEApp } from '@maidsafe/safe-node-app/src/app';
-import { MutableData, ValueVersion } from '@maidsafe/safe-node-app/src/api/mutable';
+import { MutableData, Entries } from '@maidsafe/safe-node-app/src/api/mutable';
 import error_const from '@maidsafe/safe-node-app/src/error_const';
 
 import Md from '../src/Md';
@@ -27,45 +27,7 @@ export default class AppContainer extends Md {
     const md = await app.mutableData.fromSerial(this.serial);
 
     const entries = await md.getEntries();
-    const entries_arr: Array<{ key: Buffer, value: ValueVersion }> = await entries.listEntries();
-
-    const map = [...this.webIds.entries()];
-
-    const mutation = await entries.mutate();
-
-    for (const { key: mdKey, value: mdValue } of entries_arr) {
-      const index = map.findIndex(([key,]) => key.equals(mdKey));
-
-      // If map does not have this key.
-      if (index === -1) {
-        // If entry is not already deleted.
-        if (mdValue.buf.length > 0) {
-          // DELETE.
-          await mutation.delete(mdKey, mdValue.version + 1);
-        }
-
-        continue;
-      }
-
-      // If map value is different from entry.
-      if (!mdValue.buf.equals(map[index][1])) {
-        // UPDATE
-        await mutation.update(mdKey, mdValue.buf, mdValue.version + 1);
-      }
-
-      // Map has entry and value is equal. Do nothing.
-    }
-
-    for (const [key, value] of map) {
-      const index = entries_arr.findIndex(({ key: mdKey }) => key.equals(mdKey));
-
-      // If MD does not have this key.
-      if (index === -1) {
-        // INSERT
-        console.log('insert');
-        await mutation.insert(key, value);
-      }
-    }
+    const mutation = await mutate_from_map(entries, this.webIds);
 
     await md.applyEntriesMutation(mutation);
   }
@@ -74,7 +36,7 @@ export default class AppContainer extends Md {
     const [key, value] = [Buffer.from(wid.url), Buffer.from(wid.url)];
 
     // If the same Buffer content is already in the Map: do not add.
-    if ([...this.webIds].findIndex(([k,]) => k.equals(key))) {
+    if ([...this.webIds].findIndex(([k,]) => k.equals(key)) !== -1) {
       return;
     }
 
@@ -97,4 +59,54 @@ export default class AppContainer extends Md {
 
     return new this(nat.name, nat.typeTag, serial, ids);
   }
+}
+
+/**
+ * Calculate differences between Entries and Map<Buffer, Buffer>.
+ *
+ * @param entries
+ * @param entriesMap
+ * @returns Mutation that will make Entries mirror the Map.
+ */
+async function mutate_from_map(entries: Entries, entriesMap: Map<Buffer, Buffer>) {
+  const entries_arr = await entries.listEntries();
+  const map_arr = [...entriesMap.entries()];
+
+  const mutation = await entries.mutate();
+
+  for (const { key: mdKey, value: mdValue } of entries_arr) {
+    const index = map_arr.findIndex(([key,]) => key.equals(mdKey));
+
+    // If map does not have this key.
+    if (index === -1) {
+      // If entry is not already deleted.
+      if (mdValue.buf.length > 0) {
+        // DELETE.
+        await mutation.delete(mdKey, mdValue.version + 1);
+      }
+
+      continue;
+    }
+
+    // If map value is different from entry.
+    if (!mdValue.buf.equals(map_arr[index][1])) {
+      // UPDATE
+      await mutation.update(mdKey, map_arr[index][1], mdValue.version + 1);
+    }
+
+    // Map has entry and value is equal. Do nothing.
+  }
+
+  for (const [key, value] of map_arr) {
+    const index = entries_arr.findIndex(({ key: mdKey }) => key.equals(mdKey));
+
+
+    // If MD does not have this key.
+    if (index === -1) {
+      // INSERT
+      await mutation.insert(key, value);
+    }
+  }
+
+  return mutation;
 }
--- test/Identity.spec.ts
+++ test/Identity.spec.ts
@@ -12,14 +12,44 @@ describe('Identity manager', () => {
     app = await h.get_app();
   });
 
+  // it.only('tests stupid things', async () => {
+  //   class EntriesMap extends Map<Buffer, Buffer> {
+  //     set(key: Buffer | string, value: Buffer | string) {
+  //       // Convert non-Buffer to Buffer.
+  //       key = key instanceof Buffer ? key : Buffer.from(key);
+  //       value = value instanceof Buffer ? value : Buffer.from(value);
+
+  //       // Look for an equal Buffer Object that should be updated.
+  //       const equal = [...this].find(([k, v]) => k.equals(key as Buffer));
+  //       if (equal !== undefined) {
+  //         key = equal[0];
+  //       }
+
+  //       super.set(key, value);
+
+  //       return this;
+  //     }
+  //   }
+
+  //   const map = new EntriesMap();
+  //   map.set(Buffer.from('a'), Buffer.from('a0'));
+  //   map.set(Buffer.from('a'), Buffer.from('a1'));
+  //   map.set(Buffer.from('a'), Buffer.from('a2'));
+
+  //   console.log([...map.entries()].map(e => [e[0].toString(), e[1].toString()]));
+
+  // });
+
   it('does things', async () => {
     {
       const md = await app.auth.getOwnContainer();
       const c = await AppContainer.fromMd(md);
 
-      c.webIds.set(Buffer.from('a'), Buffer.from([0]));
-      c.webIds.set(Buffer.from('b'), Buffer.from([1]));
-      c.webIds.set(Buffer.from('c'), Buffer.from([2]));
+      c.webIds.set(Buffer.from('a'), Buffer.from('a0'));
+      c.webIds.set(Buffer.from('b'), Buffer.from('b0'));
+      c.webIds.set(Buffer.from('c'), Buffer.from('c0'));
+      c.webIds.set(Buffer.from('d'), Buffer.from('d0'));
+      c.webIds.set(Buffer.from('e'), Buffer.from('e0'));
 
       await c.commit(app);
     }
@@ -32,9 +62,22 @@ describe('Identity manager', () => {
       c.webIds.delete(key!);
 
       key = [...c.webIds.keys()].find(k => k.equals(Buffer.from('b')));
-      c.webIds.set(key!, Buffer.from([2]));
+      c.webIds.set(key!, Buffer.from('b1'));
 
-      c.webIds.set(Buffer.from('d'), Buffer.from([3]));
+      key = [...c.webIds.keys()].find(k => k.equals(Buffer.from('d')));
+      c.webIds.set(key!, Buffer.from('d1'));
+
+      c.webIds.set(Buffer.from('f'), Buffer.from('f0'));
+
+      await c.commit(app);
+    }
+
+    {
+      const md = await app.auth.getOwnContainer();
+      const c = await AppContainer.fromMd(md);
+      console.log([...c.webIds.entries()].map(e => [e[0].toString(), e[1].toString()]));
+
+      c.webIds.set(Buffer.from('a'), Buffer.from('a1'));
 
       await c.commit(app);
     }
@@ -43,7 +86,8 @@ describe('Identity manager', () => {
       const md = await app.auth.getOwnContainer();
       const c = await AppContainer.fromMd(md);
 
-      console.log(c.webIds);
+      console.log([...c.webIds.entries()].map(e => [e[0].toString(), e[1].toString()]));
+      const test = 123;
     }
   });
 });
